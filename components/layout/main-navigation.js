f6f39246b24137b0764018f8ad55a715f0b21151
Seamoon Pandey <hackedasocial@gmail.com>
2023-01-16 02:59:19 +0545

    initial snapshot with some basic react pages

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+import classes from './main-navigation.module.css';
+
+function MainNavigation() {
+  return (
+    <header className={classes.header}>
+      <Link href='/'>
+        <div className={classes.logo}>Next Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href='/auth'>Login</Link>
+          </li>
+          <li>
+            <Link href='/profile'>Profile</Link>
+          </li>
+          <li>
+            <button>Logout</button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
