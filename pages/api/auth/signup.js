6f450b18f66405c6bdc20b5c6fb2ec47fdd118c8
Seamoon Pandey <hackedasocial@gmail.com>
2023-01-16 12:32:43 +0545

    added password hashing and database connection

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,41 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+    const data = req.body;
+
+    const { email, password } = data;
+
+    if (
+        !email ||
+        !email.includes('@') ||
+        !password ||
+        password.trim().length < 8
+    ) {
+        res
+            .status(422)
+            .json({
+                message: "Invalid input - password should also be at least 8 characters long.",
+            });
+        return;
+    }
+
+    if (req.method === "POST") {
+        const client = await connectDatabase();
+        const db = client.db();
+
+
+        const hashedPassword = await hashPassword(password);
+
+        const result = await db.collection('users').insertOne({
+            email: email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({ message: 'Created User!' });
+    }
+
+
+}
+
+export default handler;
